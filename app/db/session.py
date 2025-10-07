5c9015f344ab873ee52263b5fb0259dd4ece8bf1
AngelJS <angel.jimenez2@estudiant.uib.cat>
2025-10-07 13:24:55 +0200

    Config Angel

--- app/db/session.py
+++ app/db/session.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 MYSQL_USER = "root"
 MYSQL_PASSWORD = "1234"
 MYSQL_HOST = "localhost"
-MYSQL_PORT = "3306"
+MYSQL_PORT = "3300"
 MYSQL_DB = "aurevia"
 
 DATABASE_URL = f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
